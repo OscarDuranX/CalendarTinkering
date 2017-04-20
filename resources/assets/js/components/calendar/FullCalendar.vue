5c1f4a488a036076e6f723504a5d7a44bcc57b82
Oscar Duran <xoscarduranx@gmail.com>
2017-04-20 21:47:26 +0200

    add files vue

--- resources/assets/js/components/calendar/FullCalendar.vue
+++ resources/assets/js/components/calendar/FullCalendar.vue
@@ -1,54 +1,23 @@
 <template>
 
-    <calendar-events></calendar-events>
+    <div class="row">
+        <div class="col-md-3">
+            <calendar-events></calendar-events>
+        </div>
+        <div class="col-md-9">
+            <calendar></calendar>
+        </div>
+    </div>
 
-    <calendar></calendar>
 </template>
 
 <script>
-    import Calendar from './Calendar.vue'
-    import CalendarEvents from './CalendarEvents.vue'
-
-    export default {
-      components: [ Calendar, CalendarEvents],
-      mounted(){
-        console.log('prova')
-      }
+  import Calendar from './Calendar'
+  import CalendarEvents from './CalendarEvents'
+  export default {
+    components: [ Calendar, CalendarEvents],
+    mounted() {
+      console.log('Component Full calendar mounted.')
     }
-</script>
-
-<script>
-  $('#calendar').fullCalendar({
-    theme: true,
-    editable: true,
-    droppable: true,
-    drop: function () {
-      // is the "remove after drop" checkbox checked?
-      if ($('#drop-remove').is(':checked')) {
-        // if so, remove the element from the "Draggable Events" list
-        $(this).remove();
-      }
-    },
-    header: {
-      left: 'prev,next,today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    events: [
-      {
-        title: 'Examen',
-        start: moment().format()
-      },
-      {
-        title: 'Examen 2',
-        start: moment().add(1, 'days').format()
-      },
-      {
-        title: 'Examen 3',
-        start: '2017-04-19T11:30:00',
-        end: '2017-04-19T12:30:00',
-        allDay: false
-      }
-    ]
-  })
+  }
 </script>
